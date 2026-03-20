8ccb25cecea937cd87c15f32035c5f075f1db668
felipe-boll <felipechavesboll@gmail.com>
2026-03-20 11:22:37 -0300

    App Home.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './Home';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Felipe Chaves Boll</Text>
+      <Home />
       <StatusBar style="auto" />
     </View>
   );
